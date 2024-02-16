d97d6aac4fd356c92163bbe7bea1b688fb911758
anastasimars <sutckina.n@gmail.com>
2024-02-16 20:58:43 +0100

    Add project files.

---
+++ ToDoList/Subtask.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList
+{
+    public class Subtask
+    {
+        public long Id { get; set; }
+        [Column("subtask_title")]
+        public string SubtaskTitle { get; set; }
+        public bool Status { get; set; }
+        public long TaskId { get; set; }
+        [ForeignKey("TaskId")]
+        public Task Task { get; set; }
+        
+        public Subtask(string SubtaskTitle, bool Status,
+            Task task) {
+            ArgumentNullException.ThrowIfNull(task);
+            this.SubtaskTitle = SubtaskTitle;
+            this.Status = Status;
+            this.Task = task;
+            this.TaskId = task.Id;
+        }
+    }
+}
---
+++ ToDoList/Task.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        [Column("task_title")]
+        public string TaskTitle { get; set; }
+        [Column("task_description")]
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public bool Status { get; set; }
+        public ICollection<Subtask> Subtasks { get; set; } 
+            = new HashSet<Subtask>();
+
+        public Task(string TaskTitle, string Description, 
+            DateTime Deadline, bool Status) {
+            this.TaskTitle = TaskTitle;
+            this.Description = Description;
+            this.Deadline = Deadline;
+            this.Status = Status;
+        
+        }
+    }
+}
