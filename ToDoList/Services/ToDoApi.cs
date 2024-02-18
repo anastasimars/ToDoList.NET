e927c366ccb4ad6e34258016fadb9c25f9b00279
anastasimars <sutckina.n@gmail.com>
2024-02-18 12:22:16 +0100

    refactored  return type in methods

--- ToDoList/Services/ToDoApi.cs
+++ ToDoList/Services/ToDoApi.cs
@@ -10,11 +10,11 @@ namespace ToDoList.Services
         void AddTask(TaskDto taskDTO);
         void UpdateTask(TaskDto taskDTO, long id);
         void DeleteTask(long id);
-        TaskDto MarkTaskAsCompleted(long taskId);
+        void MarkTaskAsCompleted(long taskId);
         List<SubtaskDto> GetAllSubtasksByTaskId(long taskId);
         void AddSubtask(long taskId, SubtaskDto subtaskDTO);
         void EditSubtask(long  taskId, long subtaskId, SubtaskDto subtaskDTO);
         void DeleteSubtask(long taskId, long subtaskId);
-        SubtaskEntity MarkSubtaskAsCompleted(long taskId, long subtaskId);
+        void MarkSubtaskAsCompleted(long taskId, long subtaskId);
     }
 }
